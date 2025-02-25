60c38553f3fed125ac7b7ccf8d6eafe55d696c12
H4S0 <hasanalic10@gmail.com>
2025-02-25 22:08:04 +0100

    add core files and authform

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/input';
+import React, { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      //axios register
+    }
+
+    if (variant === 'LOGIN') {
+      // nextauth signin
+    }
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow-sm rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
+        </form>
+        <div className="border-t border-gray-300 mt-3 w-full" />
+        <div className="flex items-center justify-center gap-x-2 mt-3">
+          <div>
+            {variant === 'LOGIN' ? 'New to messenger?' : 'Alredy have account?'}
+          </div>
+          <div
+            onClick={toggleVariant}
+            className="underline underline-offset-4 cursor-pointer"
+          >
+            {variant === 'LOGIN' ? 'Create an account' : 'Login in'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
