cc7506c7b495cf9117d0f880f83e162b2eedeb7b
H4S0 <hasanalic10@gmail.com>
2025-02-26 19:32:38 +0100

    updated

---
+++ app/components/MobileFooter.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const MobileFooter = () => {
+  return <div>MobileFooter</div>;
+};
+
+export default MobileFooter;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,12 @@
-'use client';
-
 import React from 'react';
-import { signOut } from 'next-auth/react';
+import EmptyState from '../components/EmptyState';
 
-const page = () => {
-  return <button onClick={() => signOut()}>Logout</button>;
+const Users = () => {
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+  );
 };
 
-export default page;
+export default Users;
